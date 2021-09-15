4bb4a708bea907029925d02f8e3abfab44e1aa66
Dhruv <samantdhruv@gmail.com>
2021-09-15 12:28:50 +0530

    done all link started new page welcome

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -2,13 +2,13 @@ const measurment = document.querySelectorAll(".measurment-input");
 const checkAreaBtn = document.querySelector("#check-area-btn");
 const outputEl = document.querySelector("#output");
 
-function calculateMulOf(a) {
-    const square = a*a;
+function calculateMulOf(a, b) {
+    const square = a*b;
     return square;
 }
 
 function calculateArea() {
-    const square = calculateMulOf(Number(measurment[0].value));
+    const square = calculateMulOf(Number(measurment[0].value), Number(measurment[1].value));
     const finalArea = square/2;
     outputEl.textContent = "The Area is " + finalArea
 }
